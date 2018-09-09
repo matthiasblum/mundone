c01a692f6b20b0cd67762ca0fd87a838f91a6e17
Matthias Blum <mat.blum@gmail.com>
2018-09-09 15:57:55 +0100

    minor change to avoid error if wished

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import random
 
 from mundone import Task, Workflow
@@ -18,8 +19,8 @@ def multiply(a, b):
     return a * b
 
 
-def divide_error(a, b):
-    if random.random() < 0.75:
+def divide(a, b, p=0):
+    if random.random() < p:
         return 1 / 0
     return a / b
 
@@ -32,17 +33,26 @@ def goodbye():
     print('Good bye!')
 
 
-def independent():
-    print('I am rather independent...')
+def report():
+    print('So far, so good...')
+
+
+def retry():
+    if os.path.isfile('.mundone_retry'):
+        os.unlink('.mundone_retry')
+    else:
+        open('.mundone_retry', 'wt').close()
+        raise FileNotFoundError("Shame...")
 
 
 def test():
     t1 = Task(hello)
     t2 = Task(add, (1, 2), requires=('hello',))
     t3 = Task(multiply, (t2.output, 5))
-    t4 = Task(divide_error, (t3.output, 2))
-    t5 = Task(stringify, (t4.output,))
-    t6 = Task(goodbye, requires=(t5,))
-    t7 = Task(independant, requires=('multiply',))
+    t4 = Task(report, requires=('multiply',))
+    t5 = Task(divide, (t3.output, 2))
+    t6 = Task(stringify, (t5.output,))
+    t7 = Task(retry)
+    t8 = Task(goodbye, requires=(t6, "retry"))
 
-    return Workflow([t1, t2, t3, t4, t5, t6, t7])
+    return Workflow([t1, t2, t3, t4, t5, t6, t7, t8])
