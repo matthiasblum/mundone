8ded8e6b2eb2b824c612caf99b3c51f54eb2d3c5
Matthias Blum <mat.blum@gmail.com>
2018-09-10 20:56:06 +0100

    Move version on top to prevent ImportError

--- mundone/__init__.py
+++ mundone/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .task import Task
-from .workflow import Workflow
-
 __version_info__ = (0, 2, 0)
 __version__ = '.'.join(map(str, __version_info__))
+
+from .task import Task
+from .workflow import Workflow
