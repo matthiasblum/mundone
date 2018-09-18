c7922366c1f62a1afdf6888334459297e5b4d763
Matthias Blum <mat.blum@gmail.com>
2018-09-18 19:30:23 +0100

    Tidy up

--- mundone/__init__.py
+++ mundone/__init__.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version_info__ = (0, 2, 1)
+__version_info__ = (0, 2, 2)
 __version__ = '.'.join(map(str, __version_info__))
+
+import logging
 
 from .task import Task
 from .workflow import Workflow
+
+logger = logging.getLogger('mundone')
+logger.setLevel(logging.INFO)
+_ch = logging.StreamHandler()
+_ch.setFormatter(
+    logging.Formatter(
+        fmt='%(asctime)s: %(message)s',
+        datefmt='%y-%m-%d %H:%M:%S'
+    )
+)
+logger.addHandler(_ch)
+
