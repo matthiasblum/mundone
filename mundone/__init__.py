d5bb5a3e3523cd8febf6605112ef941ce3157f09
Matthias Blum <mat.blum@gmail.com>
2018-09-18 16:56:04 +0100

    Bump version

--- mundone/__init__.py
+++ mundone/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version_info__ = (0, 2, 0)
+__version_info__ = (0, 2, 1)
 __version__ = '.'.join(map(str, __version_info__))
 
 from .task import Task
