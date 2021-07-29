012ea3a2f72756e18a7243b9b1c6a2dfb9857d11
Matthias Blum <mat.blum@gmail.com>
2021-07-29 13:30:23 +0100

    Refactor pool (again)

--- mundone/pool.py
+++ mundone/pool.py
@@ -8,7 +8,7 @@ from .task import Task
 
 _TASK_REQ = "--task--"
 _PING_REQ = "--ping--"
-_STOP_REQ = "--stop--"
+_WAIT_REQ = "--wait--"
 _KILL_REQ = "--kill--"
 _OVER_RES = "--over--"
 
@@ -16,47 +16,43 @@ _OVER_RES = "--over--"
 def _manager(src: Queue, dst: Queue, max_running: int, workdir: str):
     pending = []
     running = []
-    closed = False
+    notify_when_done = False
     while True:
         try:
             action, task = src.get(block=True, timeout=15)
         except Empty:
-            action = None
-        else:
-            if action == _TASK_REQ:
-                if closed:
-                    # Pool closed: cancel task
-                    task.terminate()
-                    dst.put(task)
-                elif len(running) < max_running:
-                    # Pool not full: start task right away
-                    task.start(dir=workdir)
-                    running.append(task)
-                else:
-                    # Pool full: task needs to wait
-                    pending.append(task)
-            elif action == _STOP_REQ:
-                closed = True  # Stop accepting new tasks
-            elif action == _KILL_REQ:
-                closed = True
-                for task in running + pending:
-                    task.terminate()
+            action = task = None
 
-                running.clear()
-                pending.clear()
-                break
-        finally:
-            # Update running/pending/finished tasks
-            t = _monitor(running, pending, max_running, workdir)
-            pending, running, done = t
-            while done:
-                dst.put(done.pop(0))
+        if action == _TASK_REQ:
+            if len(running) < max_running:
+                # Pool not full: start task right away
+                task.start(dir=workdir)
+                running.append(task)
+            else:
+                # Pool full: task needs to wait
+                pending.append(task)
+        elif action == _WAIT_REQ:
+            notify_when_done = True
+        elif action == _KILL_REQ:
+            for task in running + pending:
+                task.terminate()
 
-            if action == _PING_REQ:
-                dst.put(_OVER_RES)
+            running.clear()
+            pending.clear()
+            break
 
-            if closed and not running and not pending:
-                break  # No more running/waiting tasks
+        # Update running/pending/finished tasks
+        t = _monitor(running, pending, max_running, workdir)
+        pending, running, done = t
+        while done:
+            dst.put(done.pop(0))
+
+        if action == _PING_REQ:
+            dst.put(_OVER_RES)
+        elif not pending and not running and notify_when_done:
+            # No more running/waiting tasks
+            dst.put(_OVER_RES)
+            notify_when_done = False
 
     dst.put(_OVER_RES)
 
@@ -115,15 +111,12 @@ class Pool:
 
     def as_completed(self, wait: bool = False):
         if wait:
-            self._queue_in.put((_STOP_REQ, None))
+            self._queue_in.put((_WAIT_REQ, None))
         else:
             self._queue_in.put((_PING_REQ, None))
 
         for task in iter(self._queue_out.get, _OVER_RES):
             yield task
-
-        if wait:
-            self._queue_in = None
 
     def terminate(self):
         if self._queue_in is not None:
