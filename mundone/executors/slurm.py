67886da15216bc3ff5634d9918e6a9fe826a6da9
Matthias Blum <mat.blum@gmail.com>
2024-08-30 09:24:13 +0100

    Handle case where Slurm job is not found by sacct

--- mundone/executors/slurm.py
+++ mundone/executors/slurm.py
@@ -101,7 +101,7 @@ class SlurmExecutor:
     def poll(self) -> int:
         info = self.get_jobinfo(force=True)
         state = info.get("State")
-        status = STATES.get(state, states.PENDING)
+        status = STATES.get(state, states.UNKNOWN)
 
         if status == states.RUNNING:
             self._seen_running = True
